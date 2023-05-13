0b8bb1566729dab8c661433c8d52a7fa0e98526f
Daniel Khabibullin <i6unny@gmail.com>
2023-05-14 00:02:35 +0500

    Add react build

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import './public/index.html';
+import './index.scss';
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<h1>hiho</h1>);
